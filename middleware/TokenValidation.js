b5d95b219cd51942ecc46f50847146bb29566992
Divyaansh Mertia <divyaanshmertia@gmail.com>
2022-06-10 14:40:23 +0530

    Auth for Student Reg. and renamed file

---
+++ middleware/TokenValidation.js
@@ -0,0 +1,46 @@
+const JWT = require("jsonwebtoken");
+const Admin = require("../model/Admin")
+exports.isTokenValid = (req, res, next) => {
+  try {
+    if (!req.headers.token) {
+      console.error("No token was sent");
+      return res.status(403).send("Invalid token");
+    }
+    const decodedToken = JWT.verify(req.headers.token, "CelebalSecretKey");
+    if (decodedToken.AdminID ) {
+      return next();
+    }
+    console.warn("User sent suspicious token!");
+    return res.status(417).send("Please send valid token!");
+  } catch (error) {
+    console.error("Token Validation failed.");
+    return res.status(401).send("Token invalid.");
+  }
+};
+
+exports.canAddStudent = (req, res, next) => {
+    try {
+      if (!req.headers.token) {
+        console.error("No token was sent");
+        return res.status(403).send("Invalid token");
+      }
+      const decodedToken = JWT.verify(req.headers.token, "CelebalSecretKey");
+      
+      Admin.findOne({AdminID:decodedToken.AdminID}).then((admin)=>{
+          if(!admin){
+            console.warn("User sent suspicious token!");
+            return res.status(417).send("Please send valid token!");
+          }
+          {
+            return next();
+          }
+      }).catch((error)=>{
+        return res.status(500).send(error)
+      })
+      
+      
+    } catch (error) {
+      console.error("Token Validation failed.");
+      return res.status(401).send("Token invalid.");
+    }
+  };
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const router = express();
-const middleware = require("../middleware/isValid")
+const middleware = require("../middleware/TokenValidation")
 const adminControllers = require("../controller/adminController");
-router.post("/admin/addStudent", adminControllers.createStudent);
+router.post("/admin/addStudent", middleware.canAddStudent, adminControllers.createStudent);
 router.put("/admin/update/:id", middleware.isTokenValid, adminControllers.updateAdmin);
 router.post("/admin/addMarklist",adminControllers.createMarklist )
 router.post("/admin/addAdmin", adminControllers.createAdmin)
