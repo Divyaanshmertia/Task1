e0626aa80a228cd934e73fd546e74ae03522f21d
Divyaansh Mertia <divyaanshmertia@gmail.com>
2022-06-29 15:17:37 +0530

    minor formatting

--- routes/studentRoutes.js
+++ routes/studentRoutes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const router = express();
 const middleware = require("../middleware/TokenValidation")
 const studentControllers = require("../controller/studentController");
-router.post("/student/Login", studentControllers.LoginStudent )
+router.post("/student/login", studentControllers.LoginStudent )
 router.get("/me", middleware.canViewStudent, studentControllers.getStudentData, studentControllers.getStudentMarkList);
 router.put("/student/update", middleware.canViewStudent,studentControllers.updateStudent)
-router.get("/student/result" ,middleware.canViewStudent, studentControllers.results);
+router.get("/student/results" ,middleware.canViewStudent, studentControllers.results);
 module.exports = router
